558a93c2466aefbff86aa362014573fc9f4daf8c
Abner del Cid <abner_jonathan@hotmail.com>
2022-10-19 08:40:46 -0600

    Double receives counts example

--- spec/mocking_spec.rb
+++ spec/mocking_spec.rb
@@ -6,18 +6,22 @@ class Movie
   end
 
   def start_shooting
-    actor&.fall_in_ladder
-    actor&.ready?
+    @actor&.fall_in_ladder
+    @actor&.ready?
+    @actor&.act
+    @actor&.act
   end
 end
 
 RSpec.describe Movie do
-  let(:actor) { double('StuntMan', ready?: true, fall_in_ladder: 'Oh im falling in ladders') }
+  let(:actor) { double('StuntMan', ready?: true, fall_in_ladder: 'Oh im falling in ladders', act: 'acting') }
   subject { described_class.new(actor) }
+
   describe 'shooting action' do
     it 'can fall in ladder' do
-      expect(actor).to receive(:fall_in_ladder)
-      expect(actor).to receive(:ready?)
+      expect(actor).to receive(:fall_in_ladder).once
+      expect(actor).to receive(:ready?).once
+      expect(actor).to receive(:act).twice
       subject.start_shooting
     end
   end
